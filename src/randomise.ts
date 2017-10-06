c9b7cc160a67aac0f42e8f9edb7b129259b77f3b
luke-davison <luke.warmwater6@gmail.com>
2017-10-07 12:14:38 +1300

    boilerplate + some initial scripts

---
+++ src/randomise.ts
@@ -0,0 +1,55 @@
+const numberOfTiles = 36
+const numberOfExplorers = 4
+const numberOfStartingPlaces = 12
+const minDistanceBetweenStartingPlaces = 3
+
+export function randomiseTiles(): Array<number> {
+  const sorted: Array<number> = []
+  for (let i = 1; i <= numberOfTiles; i++) {
+    sorted.push(i)
+  }
+  const randomised: Array<number> = []
+  while (sorted.length > 0) {
+    const r = Math.floor(Math.random()*sorted.length)
+    randomised.push(sorted.splice(r, 1)[0])
+  }
+  return randomised
+}
+
+export interface startingPlacement {
+  id: number,
+  degrees: number
+}
+
+export function randomiseStartingPlacements(): {temples: Array<startingPlacement>, explorers: Array<startingPlacement>} {
+  const temples: Array<startingPlacement> = []
+  for (let id = 1; id <= numberOfExplorers; id++) {
+    let degrees = newRandomInt()
+    while (notAlreadyTaken(temples, degrees)) {
+      degrees = newRandomInt()
+    }
+    temples.push({id, degrees})
+  }
+  
+  const explorers: Array<startingPlacement> = []
+  for (let id = 1; id <= numberOfExplorers; id++) {
+    let degrees = newRandomInt()
+    while (notAlreadyTaken(explorers, degrees) && notTooCloseToTemple(id, degrees)) {
+      degrees = newRandomInt()
+    }
+    explorers.push({id, degrees})
+  }
+  return {temples, explorers}
+
+  
+  function notAlreadyTaken(arr: Array<startingPlacement>, degrees): boolean {
+    return !!arr.find(placement => placement.degrees === degrees)
+  }
+  function notTooCloseToTemple(id: number, degrees: number): boolean {
+    const temple = temples.find(temple => temple.id === id)
+    return degrees + temple.degrees < minDistanceBetweenStartingPlaces || numberOfStartingPlaces * 2 - degrees - temple.degrees < minDistanceBetweenStartingPlaces
+  }
+  function newRandomInt(): number {
+    return Math.floor(Math.random() * numberOfStartingPlaces)
+  }
+}
---
+++ src/tiles.ts
@@ -0,0 +1,84 @@
+export interface Tile {
+  id: number,
+  gem?: number,
+  paths: Array<number>
+}
+
+export function getTileById(id: number): Tile {
+  switch (id) {
+    case 1:
+      return {id, paths: [1, 3]}
+    case 2:
+      return {id, paths: [1, 3]}
+    case 3:
+      return {id, gem: 1, paths: [1, 2]}
+    case 4:
+      return {id, gem: 1, paths: [2, 3]}
+    case 5:
+      return {id, paths: [1, 2, 3]}
+    case 6:
+      return {id, paths: [1, 2, 3]}
+    case 7:
+      return {id, paths: [0, 1, 2, 3]}
+    case 8:
+      return {id, paths: [0, 1, 2, 3]}
+    case 9:
+      return {id, gem: 1, paths: [0, 1]}
+    case 10:
+      return {id, gem: 1, paths: [0, 3]}
+    case 11:
+      return {id, gem: 1, paths: [0, 1, 3]}
+    case 12:
+      return {id, paths: [0, 1, 3]}
+    case 13:
+      return {id, gem: 1, paths: [1, 3]}
+    case 14:
+      return {id, gem: 2, paths: [1, 3]}
+    case 15:
+      return {id, gem: 1, paths: [1, 3]}
+    case 16:
+      return {id, gem: 2, paths: [1, 3]}
+    case 17:
+      return {id, paths: [0, 2]}
+    case 18:
+      return {id, paths: [0, 2]}
+    case 19:
+      return {id, paths: [1, 3]}
+    case 20:
+      return {id, paths: [1, 3]}
+    case 21:
+      return {id, paths: [1, 2]}
+    case 22:
+      return {id, paths: [2, 3]}
+    case 23:
+      return {id, paths: [0, 1, 2]}
+    case 24:
+      return {id, paths: [0, 2, 3]}
+    case 25:
+      return {id, paths: [0, 1, 2, 3]}
+    case 26:
+      return {id, paths: [0, 1, 2, 3]}
+    case 27:
+      return {id, paths: [0, 1]}
+    case 28:
+      return {id, paths: [0, 3]}
+    case 29:
+      return {id, paths: [0, 1, 2]}
+    case 30:
+      return {id, paths: [0, 2, 3]}
+    case 31:
+      return {id, gem: 1, paths: [0, 2]}
+    case 32:
+      return {id, gem: 2, paths: [0, 2]}
+    case 33:
+      return {id, gem: 1, paths: [0, 2]}
+    case 34:
+      return {id, gem: 2, paths: [0, 2]}
+    case 35:
+      return {id, paths: [0, 2]}
+    case 36:
+      return {id, paths: [0, 2]}
+    default:
+      return undefined
+  }
+}
