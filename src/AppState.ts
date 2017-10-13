79e8fa319b76d200a29b7ab7a423fbc37bc274cb
luke-davison <luke.warmwater6@gmail.com>
2017-10-13 13:10:42 +1300

    adjusted placement functions

--- src/AppState.ts
+++ src/AppState.ts
@@ -10,6 +10,7 @@ class AppState {
   @observable currentPlacement: TilePlacement
   @observable player: Player
   @observable movementToggled: boolean
+  @observable tilePlaced: boolean
 
   constructor() {
     this.players = dummyPlayerData
@@ -17,31 +18,49 @@ class AppState {
     this.templePositions = dummyTemplePlacements
     this.currentPlacement = dummyCurrentPlacement
     this.player = dummyPlayerData[0]
+
+    this.tilePlaced = false
     this.movementToggled = false
 
     this.clickCell = this.clickCell.bind(this)
     this.hoverOverCell = this.hoverOverCell.bind(this)
+    this.leaveCell = this.leaveCell.bind(this)
     this.toggleMovement = this.toggleMovement.bind(this)
     this.confirmTurn = this.confirmTurn.bind(this)
   }
 
   clickCell(x: number, y: number) {
-    if (!this.player.tilePlacements.find(tilePlacement => tilePlacement.x === x && tilePlacement.y === y)) {
-      this.player.tilePlacements.push({tile: this.currentPlacement.tile, x, y})
-      this.currentPlacement = {tile: this.remainingTiles.pop(), x, y}
+    if (this.tilePlaced && this.currentPlacement.x === x && this.currentPlacement.y === y) {
+      this.tilePlaced = false
+    } else {
+      if (!this.player.tilePlacements.find(tilePlacement => tilePlacement.x === x && tilePlacement.y === y)) {
+        this.currentPlacement.x = x
+        this.currentPlacement.y = y
+        this.tilePlaced = true
+      }
     }
   }
   hoverOverCell(x: number, y: number) {
-    if (!this.movementToggled) {
+    if (!this.movementToggled && !this.tilePlaced) {
       this.currentPlacement.x = x
       this.currentPlacement.y = y
+    }
+  }
+  leaveCell(x: number, y: number) {
+    if (!this.tilePlaced && this.currentPlacement.x === x && this.currentPlacement.y === y) {
+      this.currentPlacement.x = 0
+      this.currentPlacement.y = 0
     }
   }
   toggleMovement() {
     this.movementToggled = !this.movementToggled 
   }
   confirmTurn() {
-
+    if (this.tilePlaced) {
+      this.player.tilePlacements.push({tile: this.currentPlacement.tile, x: this.currentPlacement.x, y: this.currentPlacement.y})
+      this.currentPlacement = {tile: this.remainingTiles.pop(), x: 0, y: 0}
+      this.tilePlaced = false
+    }
   }
 }
 
--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -11,6 +11,7 @@ export class Cell extends React.Component<{player: Player, x: number, y: number}
     super()
     this.clickCell = this.clickCell.bind(this)
     this.hoverOverCell = this.hoverOverCell.bind(this)
+    this.leaveCell = this.leaveCell.bind(this)
   }
   clickCell(e: any) {
     appState.clickCell(this.props.x, this.props.y)
@@ -18,17 +19,24 @@ export class Cell extends React.Component<{player: Player, x: number, y: number}
   hoverOverCell(e: any) {
     appState.hoverOverCell(this.props.x, this.props.y)
   }
+  leaveCell(e: any) {
+    appState.leaveCell(this.props.x, this.props.y)
+  }
 
   render() {
     const placement = this.props.player.tilePlacements.find((tile) => tile.x === this.props.x && tile.y === this.props.y)
     const proposedPlacement = !placement && appState.currentPlacement.x === this.props.x && appState.currentPlacement.y === this.props.y
+    let proposedClassName = ''
+    if (proposedPlacement) {
+      proposedClassName += ' darken'
+    }
     return (
-      <div className='player-cell'>
+      <div className={'player-cell' + proposedClassName}>
         {this.props.player.id === 1 && (
-          <input className='cell-button' onClick={this.clickCell} onMouseEnter={this.hoverOverCell}/>
+          <button className='cell-button' onClick={this.clickCell} onMouseEnter={this.hoverOverCell} onMouseLeave={this.leaveCell}/>
         )}
         {placement && <img src={`/static/image/tile${placement.tile.id}.jpeg`} className='tile-image' />}
-        {proposedPlacement && <img src={`/static/image/tile${appState.currentPlacement.tile.id}.jpeg`} className='tile-image' />}
+        {proposedPlacement && <img src={`/static/image/tile${appState.currentPlacement.tile.id}.jpeg`} className='proposed-tile' />}
       </div>
     )
   }
